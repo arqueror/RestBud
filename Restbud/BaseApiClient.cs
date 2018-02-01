5fdf4bc0b2670c68ae01e4ba6349667646315330
Ricardo Vasquez <arquero_r@hotmail.com>
2018-02-01 01:19:46 -0600

    Update BaseApiClient.cs
    Refactored to generic methods and added comments

--- Restbud/BaseApiClient.cs
+++ Restbud/BaseApiClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
@@ -14,7 +14,10 @@ using Newtonsoft.Json;
 
 namespace Restbud
 {
-    public abstract class BaseApiClient
+    /// <summary>
+    /// Provides methods for GET,POST,PUT,DELETE operations
+    /// </summary>
+    public class BaseApiClient
 
     {
         protected string _endpoint;
@@ -24,25 +27,27 @@ namespace Restbud
 
         private dynamic _bearerModel =null;
         public string _bearerToken { get; private set; }
-
-        public void SetTokenResponseModel<T>(T bearerModel) where T:class
+        /// <summary>
+        /// Specifies bearer token type. This will be used later by GetBearerToken method when called internally to save a temp variable with token information for later calls
+        /// </summary>
+        /// <param name="type">BearerToken type</param>
+        public void SetTokenResponseType(Type type)
         {
-            _bearerModel = bearerModel;
+            _bearerModel = Convert.ChangeType(_bearerModel, type);
         }
-        public void SetRequestParameters(string _apiUrl, string user="", string pwd="")
+        /// <summary>
+        /// Sets default service endpoint for current request.
+        /// </summary>
+        /// <param name="_apiUrl"></param>
+        /// <param name="bearerToken">bearer token</param>
+        /// <param name="user">username: Use if you dont have bearerToken yet</param>
+        /// <param name="pwd">password:  Use if you dont have bearerToken yet</param>
+        public void SetRequestParameters(string _apiUrl, string bearerToken="", string user="", string pwd="")
         {
 
             _userName = user;
+            _bearerToken = bearerToken;
             _password = pwd;
-            _endpoint = _apiUrl;
-
-        }
-
-        public void SetRequestParameters(string _apiUrl, string bearerToken, string user = "", string pwd = "")
-        {
-            _userName = user;
-            _password = pwd;
-            _bearerToken =bearerToken;
             _endpoint = _apiUrl;
 
         }
@@ -50,46 +55,46 @@ namespace Restbud
         {
             //Initialize
         }
-
-        public async Task<T> GetAsync<T>(string id)
+        /// <summary>
+        /// Performs a GET operation passing specified resource id
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="id">resource id</param>
+        /// <returns>object</returns>
+        public T Get<T>(string id)
         {
-            using (HttpClient httpClient = NewHttpClient().Result)
+            using (HttpClient httpClient =NewHttpClient().Result)
             {
                 HttpResponseMessage response;
                 response = httpClient.GetAsync(String.Concat(_endpoint, "/", id)).Result;
+                var data = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+        }
+        /// <summary>
+        /// Performs an async GET operation passing specified resource id
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="id">resource id</param>
+        /// <returns>object</returns>
+        public async Task<T> GetAsync<T>(string id)
+        {
+            using (HttpClient httpClient = await NewHttpClient())
+            {
+                HttpResponseMessage response;
+                response =await httpClient.GetAsync(String.Concat(_endpoint, "/", id));
 
                 var data = await response.Content.ReadAsStringAsync();
                 //return data as T;
                 return JsonConvert.DeserializeObject<T>(data);
             }
         }
-        public async Task<T> GetAsync<T>(int top = 0, int skip = 0)
-        {
-            using (HttpClient httpClient = NewHttpClient().Result)
-            {
-                string endpoint = _endpoint + "?";
-                List<string> parameters = new List<string>();
-                if (top > 0) parameters.Add(string.Concat("$top=", top));
-                if (skip > 0) parameters.Add(string.Concat("$skip=", skip));
-                endpoint += string.Join("&", parameters);
-                HttpResponseMessage response = await httpClient.GetAsync(endpoint);
-                var data = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<T>(data);
-            }
-        }
-        public T Get<T>(int top = 0, int skip = 0)
-        {
-            using (HttpClient httpClient = NewHttpClient().Result)
-            {
-                string endpoint = _endpoint + "?";
-                List<string> parameters = new List<string>();
-                if (top > 0) parameters.Add(string.Concat("$top=", top));
-                if (skip > 0) parameters.Add(string.Concat("$skip=", skip));
-                endpoint += string.Join("&", parameters);
-                HttpResponseMessage response = httpClient.GetAsync(endpoint).Result;
-                return JsonConvert.DeserializeObject<T>(response.Content.ReadAsStringAsync().Result);
-            }
-        }
+        /// <summary>
+        ///  Performs a GET operation passing a list of parameters
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="parms">list of params</param>
+        /// <returns>object</returns>
         public T Get<T>(NameValueCollection parms)
         {
             using (HttpClient httpClient = NewHttpClient().Result)
@@ -98,16 +103,27 @@ namespace Restbud
                 return JsonConvert.DeserializeObject<T>(response.Content.ReadAsStringAsync().Result);
             }
         }
+        /// <summary>
+        ///  Performs an async GET operation passing a list of parameters
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="parms">list of params</param>
+        /// <returns>object</returns>
         public async Task<T> GetAsync<T>(NameValueCollection parms)
         {
-            using (HttpClient httpClient = NewHttpClient().Result)
+            using (HttpClient httpClient =await NewHttpClient())
             {
                 HttpResponseMessage response = await httpClient.GetAsync(String.Concat(_endpoint, ToQueryString(parms)));
                 var data = await response.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<T>(data);
             }
         }
-        //Just returns if was success or not
+        /// <summary>
+        ///  Performs a POST operation passing an object
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="data">object to send in request</param>
+        /// <returns>true if request succeeded</returns>
         public bool Post<T>(T data)
         {
             using (HttpClient httpClient = NewHttpClient().Result)
@@ -120,9 +136,15 @@ namespace Restbud
                 return result.IsSuccessStatusCode;
             }
         }
+        /// <summary>
+        ///  Performs an async POST operation passing an object
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="data">object to send in request</param>
+        /// <returns>true if request succeeded</returns>
         public async Task<bool> PostAsync<T>(T data)
         {
-            using (HttpClient httpClient = NewHttpClient().Result)
+            using (HttpClient httpClient = await NewHttpClient())
             {
                 String obj = JsonConvert.SerializeObject(data);
                 HttpRequestMessage requestMessage = new HttpRequestMessage();
@@ -132,7 +154,12 @@ namespace Restbud
                 return result.IsSuccessStatusCode;
             }
         }
-        //Parses response into custom object type
+        /// <summary>
+        ///  Performs a POST operation passing an object
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="data">object to send in request</param>
+        /// <returns>object</returns>
         public T Post<T>(object data) where T : class
         {
             using (HttpClient httpClient = NewHttpClient().Result)
@@ -146,9 +173,15 @@ namespace Restbud
                 return parsedResponse;
             }
         }
+        /// <summary>
+        ///  Performs an async POST operation passing an object
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="data">object to send in request</param>
+        /// <returns>object</returns>
         public async Task<T> PostAsync<T>(object data) where T : class
         {
-            using (HttpClient httpClient = NewHttpClient().Result)
+            using (HttpClient httpClient = await NewHttpClient())
             {
                 String obj = JsonConvert.SerializeObject(data);
                 HttpRequestMessage requestMessage = new HttpRequestMessage();
@@ -160,31 +193,53 @@ namespace Restbud
                 return parsedResponse;
             }
         }
-        public string Put<T>(string id, T data)
+        /// <summary>
+        /// Performs a PUT operation passing in resource id and object
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="id">resource id</param>
+        /// <param name="data">object containing updated resource data</param>
+        /// <returns>object</returns>
+        public T Put<T>(string id, T data)
         {
             using (HttpClient httpClient = NewHttpClient().Result)
             {
                 HttpRequestMessage requestMessage = new HttpRequestMessage();
                 requestMessage.Method = HttpMethod.Put;
                 requestMessage.Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
-                HttpResponseMessage result = httpClient.PutAsync(String.Concat(_endpoint, "/", id), requestMessage.Content).Result;
-                return result.Content.ReadAsStringAsync().Result;
+                HttpResponseMessage response = httpClient.PutAsync(String.Concat(_endpoint, "/", id), requestMessage.Content).Result;
+                var returnedObject = response.Content.ReadAsStringAsync().Result;
+                var parsedResponse = JsonConvert.DeserializeObject<T>(returnedObject);
+                return parsedResponse;
             }
         }
-        public async Task<string> PutAsync<T>(string id, T data)
+        /// <summary>
+        /// Performs an async PUT operation passing in resource id and object
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="id">resource id</param>
+        /// <param name="data">object containing updated resource data</param>
+        /// <returns>object</returns>
+        public async Task<T> PutAsync<T>(string id, T data)
         {
-            using (HttpClient httpClient = NewHttpClient().Result)
+            using (HttpClient httpClient = await NewHttpClient())
             {
                 HttpRequestMessage requestMessage = new HttpRequestMessage();
                 requestMessage.Method = HttpMethod.Put;
                 requestMessage.Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
                 HttpResponseMessage result = await httpClient.PutAsync(String.Concat(_endpoint, "/", id), requestMessage.Content);
                 var returnedObject = await result.Content.ReadAsStringAsync();
-                return returnedObject;
+                var parsedResponse = JsonConvert.DeserializeObject<T>(returnedObject);
+                return parsedResponse;
             }
         }
-
-        public string Put<T>(T data)
+        /// <summary>
+        /// Performs a PUT operation passing in resource data
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="data">object containing updated resource data</param>
+        /// <returns>object</returns>
+        public T Put<T>(T data)
         {
             using (HttpClient httpClient = NewHttpClient().Result)
             {
@@ -193,35 +248,60 @@ namespace Restbud
                 requestMessage.Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8,
                     "application/json");
                 HttpResponseMessage result = httpClient.PutAsync(_endpoint, requestMessage.Content).Result;
-                return result.Content.ReadAsStringAsync().Result;
+                var returnedObject =  result.Content.ReadAsStringAsync().Result;
+                var parsedResponse = JsonConvert.DeserializeObject<T>(returnedObject);
+                return parsedResponse;
             }
         }
-        public async Task<string> PutAsync<T>(T data)
+        /// <summary>
+        /// Performs an async PUT operation passing in resource data
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="data">object containing updated resource data</param>
+        /// <returns>object</returns>
+        public async Task<T> PutAsync<T>(T data)
         {
-            using (HttpClient httpClient = NewHttpClient().Result)
+            using (HttpClient httpClient =await NewHttpClient())
             {
                 HttpRequestMessage requestMessage = new HttpRequestMessage();
                 requestMessage.Method = HttpMethod.Put;
                 requestMessage.Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
                 HttpResponseMessage result = await httpClient.PutAsync(_endpoint, requestMessage.Content);
-                var returnedObject = await result.Content.ReadAsStringAsync();
-                return returnedObject;
+                var returnedObject =await result.Content.ReadAsStringAsync();
+                var parsedResponse = JsonConvert.DeserializeObject<T>(returnedObject);
+                return parsedResponse;
             }
         }
-        public string Delete(string id)
+        /// <summary>
+        /// Performs a DELETE operation passing resource id
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="id">resource id</param>
+        /// <returns>object</returns>
+        public T Delete<T>(string id)
         {
             using (var httpClient = NewHttpClient().Result)
             {
                 HttpResponseMessage result = httpClient.DeleteAsync(String.Concat(_endpoint, "/", id)).Result;
-                return result.Content.ToString();
+                var returnedObject =  result.Content.ReadAsStringAsync().Result;
+                var parsedResponse = JsonConvert.DeserializeObject<T>(returnedObject);
+                return parsedResponse;
             }
         }
-        public async Task<string> DeleteAsync(string id)
+        /// <summary>
+        /// Performs an async DELETE operation passing resource id
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="id">resource id</param>
+        /// <returns>object</returns>
+        public async Task<T> DeleteAsync<T>(string id)
         {
-            using (var httpClient = NewHttpClient().Result)
+            using (HttpClient httpClient = await NewHttpClient())
             {
                 HttpResponseMessage result = await httpClient.DeleteAsync(String.Concat(_endpoint, "/", id));
-                return result.Content.ToString();
+                var returnedObject = await result.Content.ReadAsStringAsync();
+                var parsedResponse = JsonConvert.DeserializeObject<T>(returnedObject);
+                return parsedResponse;
             }
         }
         protected async Task<HttpClient> NewHttpClient()
@@ -242,10 +322,19 @@ namespace Restbud
             var queryString = "?" + string.Join("&", Array.ConvertAll(nvc.AllKeys, key => string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(nvc[key]))));
             return queryString;
         }
-        public async Task<T> GetBearerToken<T>(string siteUrl, string Username, string Password,string tokenAction="token") where T:class
+        /// <summary>
+        /// Gets bearer token for specified service using username and password
+        /// </summary>
+        /// <typeparam name="T">response type</typeparam>
+        /// <param name="endpointUrl">endpoint url</param>
+        /// <param name="Username">username</param>
+        /// <param name="Password">password</param>
+        /// <param name="tokenAction">Optional: default route for bearer token is /token</param>
+        /// <returns></returns>
+        public async Task<T> GetBearerToken<T>(string endpointUrl, string Username, string Password,string tokenAction="token") where T:class
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(siteUrl);
+            client.BaseAddress = new Uri(endpointUrl);
             client.DefaultRequestHeaders.Accept.Clear();
 
             HttpContent requestContent = new StringContent("grant_type=password&username=" + Username + "&password=" + Password, Encoding.UTF8, "application/x-www-form-urlencoded");
